a992bb61496a644da358c8e7cf9101d01b2779fb
CarlosDanielBenitez <cdanibenz@gmail.com>
2023-11-21 16:21:32 -0300

    Features: create inteerfaces to date values

---
+++ basics/interfaces.ts
@@ -0,0 +1,12 @@
+interface Person {
+    name: string,
+    code: string | number,
+    charge: number,
+    description?: string
+}
+
+let persona: Person ={
+    name: "Dani",
+    code: "01",
+    charge: 1,
+}
